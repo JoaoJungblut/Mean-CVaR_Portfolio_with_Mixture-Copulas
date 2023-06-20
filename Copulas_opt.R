a765ee0b89edecaae98b4abf8d93d958d8f9bca1
JoaoJungblut <joaoramosjungblut@hotmail.com>
2023-06-20 14:26:36 -0300

    Update Copulas_opt.R

--- Copulas_opt.R
+++ Copulas_opt.R
@@ -8,63 +8,31 @@ library(Rsolnp)
 # importing data and calculating returns
 returns <- tq_get(c("PETR4.SA", "VALE3.SA", "ITUB4.SA",
                     "BBAS3.SA", "ABEV3.SA"), 
-                  from="2020-01-01") %>% 
+                  from="1997-01-01") %>% 
   select(date, symbol, adjusted) %>% 
   group_by(symbol) %>% 
-  mutate(return = log(adjusted) - log(lag(adjusted))) %>% 
+  mutate(return = log(adjusted) - log(dplyr::lag(adjusted))) %>% 
   ungroup() %>% 
   select(-adjusted) %>% 
   spread(symbol, return) %>% 
   na.omit()
 
 # creating auxiliary matrices and list
-n_assets <- ncol(returns)-1
-data <- cbind(returns)
-residuals <- matrix(nrow = nrow(data) - 250, ncol = ncol(data) - 1)
-sigma <- matrix(nrow = nrow(data) - 250, ncol = ncol(data) - 1)
-coef <- vector("list", 8)
-unif_dist <- matrix(nrow = nrow(data) - 250, ncol = ncol(data) - 1)
+N <- ncol(returns)-1
+ymd_vector <- seq(ymd(paste0(2001, "-01-01")), ymd(paste0(2023, "-01-01")), by = "year")
+names_vector <- names(returns)[-1]
+unif_dist <- coef <- sigma <- residuals <- vector("list", length(ymd_vector)-1)
 
-# defining interval of estimation
-t1 <- 1
-t2 <- nrow(data)-250
 
-# iterating over assets
-for(i in 2:ncol(data)){
-  print(i)
-  x <- data[t1:t2, i]
-  
-  # estimating garch  
-  mod_garch <- try(ugarchspec(variance.model = list(model="gjrGARCH",
-                                                    garch.order=c(1,1),
-                                                    variance.targeting=TRUE),
-                              mean.model = list(arma.order=c(1,0)),
-                              distribution.model = "sstd"),
-                   silent = TRUE) 
-  
-  garch_fit <- try(ugarchfit(mod_garch, data=x, solver="hybrid"), silent=TRUE)
-  
-  # saving results  
-  residuals[,i-1] <- garch_fit@fit$residuals
-  sigma[,i-1] <- garch_fit@fit$sigma
-  coef[[i-1]] <- garch_fit@fit$coef
-  
-  # generating CDF 
-  unif_dist[,i-1] <- psstd(q = residuals[, i-1]/sigma[, i-1], 
-                     nu = coef[[i-1]][8], 
-                     xi = coef[[i-1]][7])
-  #unif[,i-1] = pobs(residuals[,i-1]/sigma[,i-1])
-}  
-
-residuals <- vector("list", nrow(returns)-250)
-sigma <- vector("list", nrow(returns)-250)
-coef <- vector("list", nrow(returns)-250)
-unif_dist <- vector("list", nrow(returns)-250)
-
-for (i in 1:(nrow(returns) - 250)) {
-  for (j in 2:ncol(returns)) {
-    x <- cbind(returns[i:(i + 249), j])
-    mod_garch <- try(ugarchspec(variance.model = list(model = "gjrGARCH",
+# iterating over n assets
+for (i in 1:(length(ymd_vector)-1)) {
+  for (j in 1:length(names_vector)) {
+    x <- returns %>% 
+      dplyr::filter(date >= ymd_vector[i] & date < ymd_vector[i+1]) %>% 
+      select(names_vector[j]) %>% 
+      as.matrix()
+    
+    mod_garch <- try(ugarchspec(variance.model = list(model = "sGARCH",
                                                       garch.order = c(1, 1),
                                                       variance.targeting = TRUE),
                                 mean.model = list(armaOrder = c(1, 0)),
@@ -74,13 +42,13 @@ for (i in 1:(nrow(returns) - 250)) {
     garch_fit <- try(ugarchfit(mod_garch, data = x, solver = "hybrid"), 
                      silent = TRUE)
     
-    residuals[[i]][[j-1]] <- garch_fit@fit$residuals
-    sigma[[i]][[j-1]] <- garch_fit@fit$sigma
-    coef[[i]][[j-1]] <- garch_fit@fit$coef
+    residuals[[i]][[j]] <- garch_fit@fit$residuals
+    sigma[[i]][[j]] <- garch_fit@fit$sigma
+    coef[[i]][[j]] <- garch_fit@fit$coef
     
-    unif_dist[[i]][[j - 1]] <- psstd(q = residuals[[i]][[j-1]]/sigma[[i]][[j-1]],
-                                     nu = coef[[i]][[j-1]][7],
-                                     xi = coef[[i]][[j-1]][6])
+    unif_dist[[i]][[j]] <- psstd(q = residuals[[i]][[j]]/sigma[[i]][[j]],
+                                     nu = coef[[i]][[j]][6],
+                                     xi = coef[[i]][[j]][5])
     #unif[[i]][[j - 1]] = pobs(residuals[[i]][[j-1]]/sigma[[i]][[j-1]])
   }
 }
@@ -114,9 +82,9 @@ eqfun <- function(params, U, copC, copG, copt){
 cop_param <- vector('list', length(unif_dist)) 
   
 #initializing copulas object
-copt <- tCopula(param = 0.5, dim = n_assets) 
-copC <- claytonCopula(2, dim = n_assets) # delta= 2
-copG <- gumbelCopula(2, dim = n_assets)  # theta= 2
+copt <- tCopula(param = 0.5, dim = N) 
+copC <- claytonCopula(2, dim = N) # delta= 2
+copG <- gumbelCopula(2, dim = N)  # theta= 2
   
 #lower and upper bounds of the parameters and weights for bounded non linear opt.
 lower <- c(0.1, 1, -0.9,(2+.Machine$double.eps), 0,0,0)     
@@ -126,7 +94,7 @@ upper <- c(copC@param.upbnd, copG@param.upbnd, 1,100, 1,1,1) #2+eps so that vari
 for(i in 1:length(unif_dist)){ 
   ##pseudo-uniform [0,1] observations for each asset
   v<-as.matrix(do.call(cbind, unif_dist[[i]]))
-  U<-v[,1:(n_assets)]  
+  U<-v[,1:(N)]  
     
   ##Creating elliptical copula objects and estimating "initial guesses" for each copula parameter. 
   #Then, we maximize loglikelihood of the linear combination of the three copulas
@@ -150,28 +118,28 @@ for(i in 1:length(unif_dist)){
 # function to generate mixture copulas for potfolio optimization
 mix_cop_sim <- function(cop_pars, i, nsim){
   #clayton, t, gumbel variates matrix
-  Cc <- Cg <- Ct <- matrix(0,nrow = nsim, ncol = n_assets)   
-  ctg <- matrix(0, nrow = nsim, ncol = n_assets)              
+  Cc <- Cg <- Ct <- matrix(0,nrow = nsim, ncol = N)   
+  ctg <- matrix(0, nrow = nsim, ncol = N)              
   ##generating copula variates
   Cc[,]<- cop_pars[[i]][[5]]*rCopula(n = nsim, 
                                      copula = claytonCopula(param = cop_pars[[i]][[1]], 
-                                                            dim = n_assets))   
+                                                            dim = N))   
   Cg[,]<- cop_pars[[i]][[6]]*rCopula(n = nsim, 
                                      copula = gumbelCopula(param = cop_pars[[i]][[2]], 
-                                                           dim = n_assets))
+                                                           dim = N))
   Ct[,]<- cop_pars[[i]][[7]]*rCopula(n = nsim, 
                                      copula = tCopula(param = cop_pars[[i]][[3]], 
                                                       df = cop_pars[[i]][[4]], 
-                                                      dim = n_assets))
+                                                      dim = N))
   ctg <- Cc + Ct + Cg #linear combination of them 
   return(ctg)
 }
 
 gauss_cop_sim <- function(cop_pars, i, nsim){
-  Gcop <- matrix(0, nrow = nsim, ncol = n_assets)
+  Gcop <- matrix(0, nrow = nsim, ncol = N)
   Gcop[,] <- copula::rCopula(n = nsim, 
                              copula = normalCopula(param = cop_pars[[i]], 
-                                                   dim = n_assets))
+                                                   dim = N))
   return(Gcop)
 }
 
@@ -179,7 +147,7 @@ gauss_cop_sim <- function(cop_pars, i, nsim){
 z_cop_sim <- function(garch_coefs, ctg, i, nsim){
   zsim <- matrix(0, nrow = nsim, ncol = 8) 
   #for each asset, generate copula 'z' dependence strucutre 
-  for(j in 1:n_assets){  
+  for(j in 1:N){  
     zsim[,j] <- qsstd(ctg[,j], 
                       nu = garch_coefs[[i]][[j]][[8]], 
                       xi = garch_coefs[[i]][[j]][[7]]) / # garch_coefs[[i]][[j]][[7]]
@@ -194,7 +162,7 @@ ret_cop_sim <- function(garch_coefs, zsim, sigma_per, resid_per, returns, i, nsi
   ret_sim <- matrix(0, nrow = nsim, ncol = 8)    
   #getting real returns we'll use in one-step forward armaGarch's AR forecasting
   RZ <- returns[i:(1259+i),2:9] 
-  for(j in 1:n_assets){   #K scenarios generation for each asset
+  for(j in 1:N){   #K scenarios generation for each asset
     sigma_f_t1 <- tail(sigma_per[[j]],1) ##(t-1) sigma for GARCH forecasting
     e_f_t2_t1 <- tail(resid_per[[j]],2) ##(t-2, t-1) residuals for MA forecasting
     for(z in 1:nsim){
