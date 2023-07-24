cf386463881803f2c8657c0555dc28552e99c270
João Jungblut <joaoramosjungblut@hotmail.com>
2023-07-24 11:42:07 -0300

    Update main.R

--- main.R
+++ main.R
@@ -8,9 +8,11 @@
 my_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(my_dir)
 
+
 # cleaning variables and graphs
 rm(list=ls())
 graphics.off()
+
 
 # Load required packages
 library(tidyverse)     # Data manipulation and visualization
@@ -23,6 +25,7 @@ library(fPortfolio)    # Portfolio optimization
 library(PerformanceAnalytics) # Performance metrics
 library(xts) # Time series object
 
+
 # Importing modules
 source("data_preprocessing.R")
 source("garch_estimate.R")
@@ -31,34 +34,44 @@ source("portfolio_optimization.R")
 source("model.R")
 source("performance_metrics.R")
 
+
 # Loading ETF returns
 returns <- read_csv("data_directory/etfs_rtn.csv")[-1]
+
 
 # Creating auxiliary matrices and list
 We <- 252 # Window size for GARCH estimation
 Wt <- nrow(returns) # Total size of window
 K <- 1000 # Number of simulations 
 
+
 # Construct portfolio and benchmarks
 mixture_portfolio <- RollingWindowEstimation(returns = returns,
                                              We = 252,
                                              Wt = Wt,
                                              K = K)
-
 gaussian_portfolio <- RollingWindowEstimation(returns = returns,
                                               We = 252,
                                               Wt = Wt,
                                               K = K)
-
 naive_portfolio <- NaiveDiversification(returns)
 
 
 # Convert the portfolio_returns matrix to an xts object
 mixture_portfolio_xts <- xts::xts(mixture_portfolio[,-1], 
                                   order.by = mixture_portfolio$date)
-
 gaussian_portfolio_xts <- xts::xts(gaussian_portfolio[,-1], 
                                    order.by = gaussian_portfolio$date)
-
 naive_portfolio_xts <- xts::xts(naive_portfolio[(We+1):Wt, -1],
                                 order.by = naive_portfolio[(We+1):Wt,]$date)
+
+
+# Compute Performance
+mixture_portfolio_performance <- ComputePerformance(mixture_portfolio_xts)
+gaussian_portfolio_performance <- ComputePerformance(gaussian_portfolio_xts)
+naive_portfolio_performance <- ComputePerformance(naive_portfolio_xts)
+
+# Generate graph
+PerformanceAnalytics::charts.PerformanceSummary(mixture_portfolio_xts)
+PerformanceAnalytics::charts.PerformanceSummary(gaussian_portfolio_xts)
+PerformanceAnalytics::charts.PerformanceSummary(naive_portfolio_xts)
