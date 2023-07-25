a1f2c4fed99d0fe4c1b6f63840fe48ef170827d8
João Jungblut <joaoramosjungblut@hotmail.com>
2023-07-24 21:28:51 -0300

    Update exporting_results.R

--- exporting_results.R
+++ exporting_results.R
@@ -6,22 +6,24 @@ SaveSummaryStats <- function(df, filename = "tables/summary_stats_table.txt") {
   #   df: A data frame containing asset returns with a 'date' column and individual asset columns.
   #   filename: File path to save the LaTeX table (default = "tables/summary_stats_table.txt").
   
-  library(xtable)
-  
   # Summary statistics for each ETF's log returns
-  summary_stats <- summary(df[-1]) # Exclude the date column from summary
-  skewness_values <- apply(df[-1], 2, skewness) # Calculate skewness for each column
-  kurtosis_values <- apply(df[-1], 2, kurtosis) # Calculate kurtosis for each column
-  
-  # Create a data frame to store the summary statistics
-  summary_df <- data.frame(Metric = c("Mean", "Std. Dev.", "Min", "1st Qu.", "Median", "3rd Qu.", "Max", "Skewness", "Kurtosis"),
-                           t(summary_stats), skewness_values, kurtosis_values)
+  df[is.na(df)] <- 0
+  summary_stats <- data.frame("Symbol" = names(df[-1]),
+                              "Min." = colMins(df[-1]),
+                              "1st Quantile" = colQuantiles(df[-1], prob = 0.25),
+                              "Median." = colQuantiles(df[-1], prob = 0.5),
+                              "Mean" = colMeans(df[-1]),
+                              "3rd Quantile" = colQuantiles(df[-1], prob = 0.75),
+                              "Max." = colMaxs(df[-1]),
+                              "Std. Dev" = colSds(df[-1]),
+                              "Skewness" = colSkewness(df[-1]),
+                              "Kurtosis" = colKurtosis(df[-1]))[-1]
   
   # Convert the data frame to a LaTeX table using xtable
-  summary_table <- xtable(summary_df,
+  summary_table <- xtable(summary_stats,
                           caption = "Summary Statistics of ETF Returns",
-                          align = c("l", "c", "c", "c", "c", "c", "c", "c", "c"),
-                          digits = c(2, 4, 4, 4))
+                          align = c("l", "c", "c", "c", "c", "c", "c", "c", "c", "c"),
+                          digits = c(0, 2, 4, 4, 4, 4, 2, 4, 4, 4))
   
   # Save the LaTeX table as .txt file
   write(as.character(summary_table), file = filename)
@@ -79,33 +81,33 @@ SaveGraphReturns <- function(df, filename = "figures/returns_figure.png") {
   #   filename: File path to save the plot as an image (default = "figures/returns_figure.png").
   
   # Convert the data from wide to long format using 'gather' 
+  df[is.na(df)] <- 0
   df_long <- tidyr::gather(df, key = "Symbol", value = "Returns", -date)
   
   colors <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-              "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf")
-  names(colors) <- unique(df_long$ETF)
+              "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
+              "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5",
+              "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d")
+  names(colors) <- unique(df_long$Symbol)
   
   # Create the plot using 'ggplot2' and 'facet_wrap'
-  p <- ggplot(df_long, aes(x = date, y = Returns)) +
+  p <- ggplot(df_long, aes(x = date, y = Returns, color = Symbol)) +
     geom_line() +
     labs(title = "ETF Returns",
          x = "Date",
          y = "Returns",
          color = "Symbol") +
-    theme_minimal() +
-    facet_wrap(~ ETF, scales = "free_y", ncol = 4) +  # Adjust the 'ncol' value as needed
+    theme_bw() +  # White background
+    facet_wrap(~ Symbol, scales = "free_y", ncol = 4) +  # Adjust the 'ncol' value as needed
     scale_color_manual(values = colors) +
     theme(strip.text = element_text(size = 12, face = "bold"),
           axis.title.x = element_text(size = 14, face = "bold"),
           axis.title.y = element_text(size = 14, face = "bold"),
-          plot.title = element_text(size = 16, face = "bold", hjust = 0.5))
-  
-  # Add title to each graph inside the facets
-  p <- p + labs(title = NULL) + facet_wrap(~ ETF, scales = "free_y", ncol = 4,
-                                           labeller = labeller(ETF = function(x) paste("ETF", x)))
+          plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+          axis.text = element_text(size = 10))  # Decrease the axis numbers font size
   
   # Save the plot as a file
-  ggsave(filename, plot = p, width = 15, height = 10, units = "cm")  # Adjust 'width' and 'height' as needed
+  ggsave(filename, plot = p)  # Adjust 'width' and 'height' as needed
   
   cat("Performance graph saved to:", filename, "\n")
 }
