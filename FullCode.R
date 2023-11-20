bd7e501efd01008db71a07b92a6ab5c5fa07a425
João Jungblut <99040917+JoaoJungblut@users.noreply.github.com>
2023-11-20 12:03:04 -0300

    Create FullCode.R

---
+++ FullCode.R
@@ -0,0 +1,119 @@
+#################################################################################
+# Main file to run worst-case CVaR portfolio optimization based mixture copulas #
+# Authors: Joao Ramos Jungblut ##################################################
+# Last update: 2023-07-24 #######################################################
+#################################################################################
+
+
+# setting R project environment
+my_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(my_dir)
+
+
+# cleaning variables and graphs
+rm(list=ls())
+graphics.off()
+
+
+# Load required packages
+library(tidyverse)     # Data manipulation and visualization
+library(tidyquant)     # Financial data analysis
+library(readxl)        # Read excel files
+library(rugarch)       # Univariate GARCH modeling
+library(fGarch)        # Multivariate GARCH modeling
+library(copula)        # Copula modeling
+library(Rsolnp)        # Nonlinear optimization
+library(fPortfolio)    # Portfolio optimization
+library(PerformanceAnalytics) # Performance metrics
+library(xts)           # Time series object
+library(timetk)        # Time series object
+library(xtable)        # Create LaTex tables
+library(ggplot2)       # Produce graph
+library("ROI")         # Optimization
+library("ROI.plugin.glpk") # Optimization
+library("ROI.plugin.quadprog") # Optimization
+library("ROI.plugin.alabama") # Optimization
+
+
+# Fetch data
+Ret <- read_excel("data_directory/StockPrice.xlsx")[-1,] %>% 
+  mutate(date = as.Date(data), # convert to date format
+         across(VALE3:WHMT3, as.numeric)) %>%  # convert to numeric format
+  select(date, everything(), -data) %>% # select date and tickers
+  gather("Ticker", "AdjClose", -date, na.rm = TRUE) %>% # adjust to panel data
+  group_by(Ticker) %>% # calculate return for each ticker
+  mutate(Ret = log(AdjClose/ dplyr::lag(AdjClose))) %>%  # compute log return
+  ungroup() %>% # stop grouping tickers
+  select(date, Ticker, Ret) %>% # selecting necessary columns
+  spread(key = "Ticker", value = "Ret", fill = 0) # transforming data 
+Ret
+
+
+# Choosing stocks on Ibovespa per year
+MarketComposition <- read_excel("data_directory/MarketComposition.xlsx") %>% 
+  mutate(date = as.Date(Data), # convert to date format
+         m = month(date, label = TRUE)) %>% # convert to numeric format
+  dplyr::filter(m == "dez") %>% # update factors 
+  select(date, everything(),-c(Data, m)) %>% # select date, month and tickers
+  gather("Ticker", "Composition", -date, na.rm=TRUE) %>% # adjust to panel data
+  group_by(date) %>% # Select tickers by last date of the month
+  reframe(Tickers = Ticker)
+MarketComposition
+
+Update <- MarketComposition %>% 
+  select(date) %>% 
+  unique() %>% # avoid repetition
+  deframe() # create vector
+Update
+
+Symbols <- map(Update, .f = function(x){ # list of vectors with Ibov tickers per year
+  MarketComposition %>% 
+    dplyr::filter(date == x) %>% # choose stocks presented on Ibov
+    select(Tickers) %>% 
+    unique() %>% # avoid repetition
+    deframe() # create vector
+})
+names(Symbols) <- Update
+Symbols
+
+
+# Filtering stocks on Ibovespa
+Ret_List <- map(Update, .f = function(x){ 
+  # Defining stocks per size in Small and Big
+  Ret %>% 
+    select(date,  Symbols[[paste(x)]]) %>% # filter Ibov stocks
+    dplyr::filter(date > x,
+           date <= x + 365)
+    filter(Ticker %in% Symbols[[paste(x)]], # filter Ibov stocks
+           date <= x + 365) %>% 
+  Size <- MarketValue %>% 
+    filter(Ticker %in% Symbols[[paste(x)]], # filter Ibov stocks 
+           m == Month,
+           date <= x) %>% 
+    select(-m) %>% 
+    group_by(Ticker) %>% # grouping by ticker to select last value
+    summarise(date = last(date),
+              MarketValue = last(MarketValue)) %>% 
+    ungroup() %>% 
+    arrange(desc(MarketValue)) %>% # sorting stocks by size 
+    filter(MarketValue > 0) %>% # exclude negative Market Value
+    mutate(Size = case_when(MarketValue >= median(MarketValue) ~ "Big",
+                            T ~ "Small")) %>% 
+    select(date, Ticker, Size) %>% 
+    arrange(Ticker)
+  
+})
+names(Classification) <- Update
+Classification
+
+
+
+
+
+
+
+
+
+
+
+
