9e3682d19320314a64c083c7c0b8b29c10f0be77
João Jungblut <99040917+JoaoJungblut@users.noreply.github.com>
2023-11-28 08:46:25 -0300

    Update test.R

--- test.R
+++ test.R
@@ -112,6 +112,63 @@ names(Ret_outofSample) <- Update + 365
 Ret_outofSample
 
 
+# Define a function to perform computations for each  window
+Pipeline <- function(inSample, outofSample, Update, copulas){
+  
+  set.seed(2023)
+  
+  # Setting my pipeline
+  Pipe <- map(Update, .f = function(x){
+    
+    # Create returns matrix
+    returns <- inSample[[paste(x)]]
+    
+    # Fit the GARCH model to the returns data
+    fit_garch <- FitGarch(returns)
+    
+    # Subset the matrix to keep only columns with complete cases
+    garch_coef <- Filter(Negate(is.null), fit_garch$garch_coef) # Filtering NULL values 
+    unif_dist <- fit_garch$unif_dist
+    unif_dist <- unif_dist[, complete.cases(t(unif_dist))] # drop Na columns
+    sigma <- fit_garch$sigma
+    returns <- returns[, complete.cases(t(sigma))] # drop invalid stocks
+    sigma <- sigma[,complete.cases(t(sigma))] # drop Na columns
+    
+    ## Generating Mixture-Copula
+    copula_mixture <- OptMixtureCopulas(unif_dist, 
+                                        K = 10000,
+                                        combination = copulas)
+    
+    # Compute simulated standardized residuals using the mixture-copula and GARCH 
+    zsim <- ComputeZSim(copula_mixture = copula_mixture, 
+                        garch_coef = garch_coef)
+    
+    # Predict future returns using the GARCH model, simulated residuals, and volatility estimates
+    ret_pred <- PredictGarch(returns = returns, 
+                             sigma = sigma,
+                             zsim = zsim,
+                             garch_coef = garch_coef)
+    ret_pred <- as.data.frame(ret_pred)
+    colnames(ret_pred) <- colnames(returns)
+    
+    # Perform CVaR optimization to determine the optimal portfolio weights
+    weights <- rep(0, ncol(returns))
+    names(weights) <- colnames(returns)
+    weights <- CVaROptimization(returns = ret_pred,
+                                Alpha = 0.05, 
+                                TargetReturn = 0,
+                                NumAssets = 16)
+    
+    # Calculate portfolio returns based on the optimal weights 
+    ret_matrix_outofsample <- outofSample[[paste(as.Date(x) + 365)]][,colnames(returns)] # select valid stocks
+    portfolio_returns <- ret_matrix_outofsample  %*%  weights
+  }) %>% 
+    bind_rows()
+  
+  return(Pipe)
+}
+
+
 # Create returns matrix
 returns <- Ret_inSample$`2021-12-31`
 
